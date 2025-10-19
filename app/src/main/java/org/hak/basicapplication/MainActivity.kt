14ba805297122535f7bbf48ea8cddd0a5628e2fc
htetaungkyawmx <htetaungkyaw372005@gmail.com>
2025-10-19 19:27:13 +0630

    fork mr,kls

--- app/src/main/java/org/hak/basicapplication/MainActivity.kt
+++ app/src/main/java/org/hak/basicapplication/MainActivity.kt
@@ -1,53 +1,55 @@
 package org.hak.basicapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Spinner
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class RegisterActivity : AppCompatActivity() {
-
-    private lateinit var etName: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var etPassword: EditText
-    private lateinit var etConfirmPassword: EditText
-    private lateinit var btnRegister: Button
-    private lateinit var tvLogin: TextView
+class MainActivity : AppCompatActivity() {
+    lateinit var btnBack: Button
+    lateinit var btnEntry: Button
+    lateinit var btnChoose: Button
+    lateinit var spinnerCity: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        etName = findViewById(R.id.etName)
-        etEmail = findViewById(R.id.etEmail)
-        etPassword = findViewById(R.id.etPassword)
-        etConfirmPassword = findViewById(R.id.etConfirmPassword)
-        btnRegister = findViewById(R.id.btnRegister)
-        tvLogin = findViewById(R.id.tvLogin)
+        btnChoose = findViewById(R.id.btnChoose)
+        spinnerCity = findViewById(R.id.spinnerCity)
+        btnEntry = findViewById(R.id.btnEntry)
 
-        btnRegister.setOnClickListener {
-            val name = etName.text.toString().trim()
-            val email = etEmail.text.toString().trim()
-            val password = etPassword.text.toString()
-            val confirmPassword = etConfirmPassword.text.toString()
+        //Spinner
+        val cities = arrayOf("Select Your City", "Yangon", "Mandalay", "Bago", "Mawlamyine")
 
-            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cities)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCity.adapter = adapter
+
+        btnChoose.setOnClickListener {
+            val selectedCity = spinnerCity.selectedItem.toString()
+            if(selectedCity == cities[0]){
+                Toast.makeText(this,"Please choose your city", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Your, selected city is $selectedCity", Toast.LENGTH_LONG).show()
             }
-
-            if (password != confirmPassword) {
-                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            Toast.makeText(this, "Registration Successful!", Toast.LENGTH_SHORT).show()
         }
 
-        tvLogin.setOnClickListener {
-            Toast.makeText(this, "Go to Login screen", Toast.LENGTH_SHORT).show()
+
+        btnEntry.setOnClickListener {
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+
     }
 }
